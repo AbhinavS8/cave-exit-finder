859385cb24f5c97935de703c4aea033fb23e872d
AbhinavS8 <abhinav.subr@gmail.com>
2025-10-15 00:39:54 +0530

    removed models and adding 2 more searches

--- src/main/java/net/dominosq/dqcaveexit/item/ModItems.java
+++ src/main/java/net/dominosq/dqcaveexit/item/ModItems.java
@@ -1,9 +1,10 @@
 package net.dominosq.dqcaveexit.item;
 
 import net.dominosq.dqcaveexit.DQCaveExit;
-//import net.dominosq.dqpetrespawn.item.custom.PetCharmItem;
-import net.dominosq.dqcaveexit.item.custom.CagedCanaryItem;
-import net.dominosq.dqcaveexit.item.custom.Canary2Item;
+import net.dominosq.dqcaveexit.item.custom.AStarItem;
+import net.dominosq.dqcaveexit.item.custom.BFSItem;
+import net.dominosq.dqcaveexit.item.custom.BeamSearchItem;
+import net.dominosq.dqcaveexit.item.custom.DFSItem;
 import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
@@ -12,11 +13,17 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(DQCaveExit.MODID);
 
-    public static final DeferredItem<Item> CAGED_CANARY = ITEMS.register("caged_canary",
-            () -> new CagedCanaryItem(new Item.Properties().durability(1)));
+    public static final DeferredItem<Item> BFS_SEARCH = ITEMS.register("bfs_search",
+            () -> new BFSItem(new Item.Properties().durability(1)));
 
-    public static final DeferredItem<Item> A_CANARY = ITEMS.register("a_canary",
-            () -> new Canary2Item(new Item.Properties().durability(1)));
+    public static final DeferredItem<Item> A_STAR_SEARCH = ITEMS.register("a_star_search",
+            () -> new AStarItem(new Item.Properties().durability(1)));
+
+    public static final DeferredItem<Item> BEAM_SEARCH = ITEMS.register("beam_search",
+            () -> new BeamSearchItem(new Item.Properties().durability(1)));
+
+    public static final DeferredItem<Item> DFS_SEARCH = ITEMS.register("dfs_search",
+            () -> new DFSItem(new Item.Properties().durability(1)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
